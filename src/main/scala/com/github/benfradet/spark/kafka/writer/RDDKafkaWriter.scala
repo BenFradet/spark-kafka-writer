ef3d9eebf2eecee93a3fcb5dcf44deeae17b2a41
BenFradet <benjamin.fradet@gmail.com>
2016-07-23 17:40:15 +0200

    rdd writer

---
+++ src/main/scala/com/github/benfradet/spark/kafka/writer/RDDKafkaWriter.scala
@@ -0,0 +1,21 @@
+package com.github.benfradet.spark.kafka.writer
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.spark.rdd.RDD
+
+import scala.reflect.ClassTag
+
+class RDDKafkaWriter[T: ClassTag](@transient rdd: RDD[T]) extends KafkaWriter[T] {
+  override def writeToKafka[K, V](
+    producerConfig: Properties,
+    transformFunc: T => ProducerRecord[K, V]
+  ): Unit =
+    rdd.foreachPartition { partition =>
+      val producer = KafkaProducerCache.getProducer[K, V](producerConfig)
+      partition
+        .map(transformFunc)
+        .foreach(record => producer.send(record))
+    }
+}
